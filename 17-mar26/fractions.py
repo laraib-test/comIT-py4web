3d2f7a9b6bf92329b84fc964628cb20d661b72ef
laraib-tester <noorlaraib4@gmail.com>
2026-03-30 13:06:53 -0600

    fractions class is created

--- 17-mar26/fractions.py
+++ 17-mar26/fractions.py
@@ -3,9 +3,60 @@
 # Comparison: == (__eq__), != (__ne__), < (__lt__), > (__gt__)
 
 class Fraction:
+    #constructor
     
     def __init__(self, num, den):
         self.num = num
         self.den = den 
         
+        #operator(s) overloading
+    def __add__(self, other):
+        new_num = self.num * other.den + other.num * self.den
+        new_den = self.den * other.den
+        return Fraction(new_num, new_den)
+    
+    def __mul__(self, other):
+        new_num = self.num * other.num
+        new_den = self.den * other.den
+        return Fraction(new_num, new_den)
+    
+    def __eq__(self, other):
+        return self.num * other.den == other.num * self.den 
+    
+    def __sub__(self, other):
+        new_num = self.num * other.den - other.num * self.den
+        new_den = self.den * other.den
+        return Fraction(new_num, new_den)
+    
+    def __truediv__(self, other):
+        new_num = self.num * other.den
+        new_den = self.den * other.num
+        return Fraction(new_num, new_den)
+    
+    def __ne__(self, other):
+        return not self.__eq__(other)
+    
+    def __lt__(self, other):
+        return self.num * other.den < other.num * self.den
+    
+    def __gt__(self, other):
+        return self.num * other.den > other.num * self.den
+    
+    def __repr__(self):
+        return f"Fraction({self.num}/ {self.den})"
+
+
+# Example usage
+if __name__ == "__main__":
+    a= Fraction(2, 3)
+    b = Fraction(4, 5) 
+    print(a, b)
+    print(a + b)
+    print(a * b)
+    print(a - b)
+    print(a / b)  
+    print(a == b)
+    print(a != b) 
+    print(a < b)
+    print(a > b)
         
